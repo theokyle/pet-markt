e84968c80f7c246b32cac1e37cc2a1ccded82ee4
Kyle Butler <theokyle@gmail.com>
2026-04-27 00:41:07 -0500

    add products on backend

---
+++ apps/pet-markt-be/src/app/products/products.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductInput } from './dto/create-product.input';
+import { UpdateProductInput } from './dto/update-product.input';
+import { PrismaService } from '../prisma/prisma.service';
+import { Product } from '../../generated/prisma/client';
+
+@Injectable()
+export class ProductsService {
+
+  constructor(private prisma: PrismaService) {}
+  create(createProductInput: CreateProductInput) {
+    return 'This action adds a new product';
+  }
+
+  findAll() {
+    return this.prisma.product.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prisma.product.findUnique({ where: { id } });;
+  }
+
+  async searchProducts(term: string): Promise<Product[]> {
+    const lowercaseTerm = term.toLowerCase();
+    return this.prisma.product.findMany({
+      where: {
+        OR: [
+          { name: { contains: lowercaseTerm, mode: 'insensitive' } },
+          { description: { contains: lowercaseTerm, mode: 'insensitive' } },
+        ],
+      },
+    });
+  }
+
+  update(id: number, updateProductInput: UpdateProductInput) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
